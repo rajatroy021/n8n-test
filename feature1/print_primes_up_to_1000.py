aa441784685843ffd90f3d14f6d2522456280406
Rajat Roy <88376610+rajatroy021@users.noreply.github.com>
2025-06-30 12:54:11 +0530

    Commiting as per Jira Description from NNDES-1

---
+++ feature1/print_primes_up_to_1000.py
@@ -0,0 +1,18 @@
+def is_prime(n):
+    if n <= 1:
+        return False
+    if n <= 3:
+        return True
+    if n % 2 == 0 or n % 3 == 0:
+        return False
+    i = 5
+    while i * i <= n:
+        if n % i == 0 or n % (i + 2) == 0:
+            return False
+        i += 6
+    return True
+
+# Print all prime numbers up to 1000
+for number in range(2, 1001):
+    if is_prime(number):
+        print(number)
